22f6665cec40c79fd491beb24a07fcc4aec30f98
Mibrai <53493202+Mibrai@users.noreply.github.com>
2023-10-11 13:15:06 +0200

    remove trash files for Enum Class Pwd_Status

--- src/main/java/com/rest/goldenekrone/userManagement/exceptions/Pwd_StatusNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.rest.goldenekrone.userManagement.exceptions;
-
-public class Pwd_StatusNotFoundException extends NotFounderClassException{
-    public Pwd_StatusNotFoundException(String message){
-        super(message);
-    }
-}
